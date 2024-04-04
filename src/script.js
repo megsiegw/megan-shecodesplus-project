02731317f2c38828e92e610bf75ea6aaa1e94621
megsiegw <walsh.megangwen@gmail.com>
2024-04-04 18:24:29 +0100

    Added search functionality using API

--- src/script.js
+++ src/script.js
@@ -1,9 +1,28 @@
+function displayWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#weather-app-city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+function searchCity(city){
+let apiKey = "c8dbd4bdo54f7b1b0a9t88a8d308b3fc";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayWeather);
+}
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#weather-app-city");
-    cityElement.innerHTML = searchInput.value;
+    
+   
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form")
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Cork");
