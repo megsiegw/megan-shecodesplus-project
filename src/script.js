08ba19927b0c496a1a65f73c7d215d49e7e247d6
megsiegw <walsh.megangwen@gmail.com>
2024-04-16 14:45:08 +0100

    Inject Forecast HTML through JS

--- src/script.js
+++ src/script.js
@@ -51,7 +51,37 @@ function handleSearchSubmit(event){
     searchCity(searchInput.value);
 }
 
+function displayForecast() {
+
+   
+
+    let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+let forecastHtml = "";
+
+    days.forEach(function(day){
+        forecastHtml = forecastHtml + `<div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class = "weather-forecast-icon"><img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" alt="" width="44px"/></div>
+        <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>15º</strong><span class="weather-forecast-dash">/</span> <span class="weather-forecast-temperature">9º</span>
+            </div>
+            </div>
+           
+        </div>`;
+    })
+
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml
+
+
+}
+
 let searchFormElement = document.querySelector("#search-form")
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Cork");
+
+displayForecast()
+
+
